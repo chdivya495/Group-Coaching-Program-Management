bc9b33f259b07a04260ac3741e0af6b2f9ef87d0
Divya chouhan <chouhandivya495@gmail.com>
2024-10-14 15:13:52 +0530

    seed data updated

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -5,6 +5,7 @@ class Coach < ApplicationRecord
   has_and_belongs_to_many :companies
   has_many :employees
 
-  validates :name, presence: true, length: { maximum: 100 }
+  validates :first_name, presence: true, length: { maximum: 100 }
+  validates :last_name, presence: true, length: { maximum: 100 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
